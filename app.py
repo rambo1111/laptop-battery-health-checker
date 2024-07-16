79d5d972eb21e89125e7e78865d90f949d29d0eb
rambo1111 <132392947+rambo1111@users.noreply.github.com>
2024-07-16 19:50:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/download/<path:filename>')
-def download(filename):
-    return send_from_directory(os.getcwd(), filename, as_attachment=True)
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
